8d025d3536f475d50ff2f8c641e14d8cd77b2084
Nicholas Carrara <ncarrara.physics@gmail.com>
2024-01-20 11:49:30 -0800

    some changes

--- scripts/minirun4/submit_minirun4_jobs.sh
+++ scripts/minirun4/submit_minirun4_jobs.sh
@@ -1,6 +1,6 @@
 # Submit the preparation job and get its job ID
 
-NUMBER_OF_ARRAKIS_PROCESSES=100
+export NUMBER_OF_ARRAKIS_PROCESSES=100
 
 minirun4_config_prep_jobid=$(sbatch minirun4_config_prep.slurm | cut -d ' ' -f 4)
 
