de4cacb1f1e0875ef459ecff2b074e48c9f0f2c8
Nicholas Carrara <ncarrara.physics@gmail.com>
2024-01-20 09:26:23 -0800

    some changes

--- scripts/minirun4/submit_minirun4_jobs.sh
+++ scripts/minirun4/submit_minirun4_jobs.sh
@@ -5,4 +5,4 @@ NUMBER_OF_ARRAKIS_PROCESSES=100
 minirun4_config_prep_jobid=$(sbatch minirun4_config_prep.slurm | cut -d ' ' -f 4)
 
 # Submit the array job with a dependency on the preparation job
-sbatch --dependency=afterok:$minirun4_config_prep_jobid minirun4_data_prep.slurm
+sbatch --dependency=afterok:$minirun4_config_prep_jobid --array=0-$NUMBER_OF_ARRAKIS_PROCESSES minirun4_data_prep.slurm
