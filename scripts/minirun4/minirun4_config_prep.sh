05fb4784d7a35255cc8c3a0fe57ea5f178b26fe9
Nicholas Carrara <ncarrara.physics@gmail.com>
2024-01-21 12:57:09 -0800

    some changes

--- scripts/minirun4/minirun4_config_prep.sh
+++ scripts/minirun4/minirun4_config_prep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /local_scratch
 
-prep_config="/workspace/ArrakisND/configs/minirun4.yaml"
+prep_config="/workspace/ArrakisND/config/minirun4.yaml"
 
 # generate arrakis configs
 create_arrakis_runs "$prep_config" -arrakis_config_location=${LOCAL_BLIP} -number_of_processes=${NUMBER_OF_ARRAKIS_PROCESSES}
